45b77ed40fad64977cb925cec71645870cb4b335
IrekJuralewicz <104641335+IrekJuralewicz@users.noreply.github.com>
2022-11-29 22:42:10 +0100

    s

--- challenge/test/ChallengeTest/TypeTests.cs
+++ challenge/test/ChallengeTest/TypeTests.cs
@@ -10,9 +10,13 @@ namespace ChallengeTest
         public void WezPracownikaZwracaRozneObiekty()
         {
             var pr1 = WezPracownika("Adam");
-            var pr2 = WezPracownika("Tomek");
-            Assert.NotSame(pr1, pr2);
-            Assert.False(Object.ReferenceEquals(pr1, pr2));
+            //var pr2 = WezPracownika("Tomek");
+            var pr2 = pr1; //przypisujemy referencję a nie wartość!!!
+
+            //Assert.NotSame(pr1, pr2);
+            //Assert.False(Object.ReferenceEquals(pr1, pr2));
+            Assert.Same(pr1, pr2);
+            Assert.True(Object.ReferenceEquals(pr1, pr2));
         }
 
         private Pracownik WezPracownika(string name)
